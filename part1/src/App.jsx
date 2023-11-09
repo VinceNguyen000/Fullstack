780313ddaaa0a62d887f6732d60e8fe2d4e04a07
VinceNguyen000 <vluxiphe@gmail.com>
2023-11-09 16:53:47 -0600

    exercise 1.1

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,35 +1,94 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
+const Header = (props) => {
+  return <h1>{props.course}</h1>;
+};
 
+const Part = (props) => {
+  return (
+    <p>
+      {props.part}: {props.exercise}
+    </p>
+  );
+};
+
+const Content = (props) => {
+  return (
+    <div>
+      {props.parts.map((element) => (
+        <Part part={element.part} exercise={element.exercise} />
+      ))}
+    </div>
+  );
+};
+
+const Total = (props) => {
+  const listTotalExercises = props.exercises;
+  let total = 0;
+  for (let i = 0; i < listTotalExercises.length; i++) {
+    total += listTotalExercises[i];
+  }
+  return <div>Number of exercises: {total}</div>;
+};
+
+const MyComponent = () => {
+  const users = ["user 1", "user 2", "user 3"];
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div>{users.map((element) => element + " ")}</div>
+      <div>{users.map((element) => element[0])}</div>
     </>
-  )
-}
+  );
+};
 
-export default App
+const App = () => {
+  const course = "Half Stack application development";
+  const part1 = "Fundamentals of React";
+  const exercises1 = 10;
+  const part2 = "Using props to pass data";
+  const exercises2 = 7;
+  const part3 = "State of a component";
+  const exercises3 = 14;
+
+  const Course = {
+    name: "Half Stack application development",
+    parts: [
+      {
+        part: "Fundamentals of React",
+        exercise: 10,
+      },
+      {
+        part: "Using props to pass data",
+        exercise: 7,
+      },
+      {
+        part: "State of a component",
+        exercise: 14,
+      },
+    ],
+  };
+
+  return (
+    <div>
+      <MyComponent />
+      <Header course={Course.name} />
+      <Content parts={Course.parts} />
+      <Total exercises={Course.parts.map((element) => element["exercise"])} />
+      <p>
+        {part1} {exercises1}
+      </p>
+      <p>
+        {part2} {exercises2}
+      </p>
+      <p>
+        {part3} {exercises3}
+      </p>
+      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+    </div>
+  );
+};
+
+export default App;
